6bcac9e91c88434daed0b45e2aff7494ef3705b2
ymlee <joo0171@naver.com>
2020-12-04 20:04:19 +0900

    복구

--- app/src/main/java/com/example/team/MainActivity.kt
+++ app/src/main/java/com/example/team/MainActivity.kt
@@ -1,27 +1,58 @@
 package com.example.team
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.team.Fragment.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.navi_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, detailViewFragment).commit()
+                return true
+            }
+            R.id.navi_map -> {
+                var mapFragment = MapFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, mapFragment).commit()
+                return true
+            }
+            R.id.navi_add -> {
+                if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
+
+                return true
+            }
+            R.id.navi_chat -> {
+                var chatFragment = ChatFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, chatFragment).commit()
+                return true
+            }
+            R.id.navi_user -> {
+                var userFrament = UserFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, userFrament).commit()
+                return true
+            }
+        }
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
 
-        val intent = Intent(this, Loading::class.java)
-        startActivity(intent)
-
-        bt_signup.setOnClickListener(){
-            val intent = Intent(this, SignUp::class.java)
-            //intent.putExtra("WMI",ed_ID.text.toString())
-            startActivity(intent)
-        }
-        bt_signin.setOnClickListener(){
-            val intent = Intent(this, Home::class.java)
-            //intent.putExtra("WMI",ed_ID.text.toString())
-            startActivity(intent)
-        }
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 }
